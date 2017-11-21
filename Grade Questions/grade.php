cae672a4e39da5ec5ba10a2e3fd3fae58e46112d
seymej72 <33495711+seymej72@users.noreply.github.com>
2017-11-20 18:05:04 -0600

    Add files via upload

---
+++ Grade Questions/grade.php
@@ -0,0 +1,23 @@
+<?php
+/* Take student submittion (Need StudentID - in a cookie?)
+     1.Store locally
+   Query database  (Need QuestionID or look for active question?)
+     1.CorrectAnswer
+     2.NumberOfCorrectAnswer
+     3.NumberOfPoints
+   Evalutate points earned based on question type
+   Upload to database
+     1.StudentSubmission
+     2.PointsEarned
+   Query database to get each students number of points
+   Calculate Average
+     1.Upload AveragePoints
+   Redirect page back to: 
+   http://webdev.cs.uwosh.edu/students/dunkod78/pindiv3/quiz_student.html
+   
+   -(Do I need to check if they have submitted a solution already)
+   -(Should be a page after submittion which shows points earned and the correct answer)
+   -How to close question? On instructors side
+   -StartTime/EndTime On Instructor side
+*/
+?>
