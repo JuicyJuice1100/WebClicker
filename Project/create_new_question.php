9211ee97a9271e40db462e9d722619d3a00edf1b
DavidDunkovich <31701641+DavidDunkovich@users.noreply.github.com>
2017-11-24 18:05:31 -0600

    generate html based on add_new_question form
    -uses new js functions to dynamically add new fields based on input values in add_new_question
    -create_new_question takes from the fields in add_new_question and creates a new question based on the form values

---
+++ Project/create_new_question.php
@@ -0,0 +1,204 @@
+<!-- Must pull from database what the question number is -->
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $question       = $_POST['question_description'];
+    $section        = $_POST['section'];
+    $subSection     = $_POST['sub_section'];
+    $subject        = $_POST['subject'];
+    $pointValue     = $_POST['point_value'];
+    $correctAnswers = $_POST['correct_answers'];
+    $startDate      = $_POST['correct_answers'];
+    $endDate        = $_POST['correct_answers'];
+    $startTime      = $_POST['correct_answers'];
+    $endTime        = $_POST['correct_answers'];
+    $questionType   = $_POST['question_type'];
+?>
+
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <title>UWO WebCLICKER - Instructor</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" media="all" href="dev.css" />
+    </head>
+<body>
+ <header>
+        <a href="questions_instructor.html">
+            <img id="logo" src="./images/logo.png"
+                   width="250" alt="UWO WebCLICKER logo" />
+        </a>
+        
+        <div class="header_user_type">Instructor</div>
+        
+        <nav>
+            <ul>
+                <li><a href="sign_in.html">Log out</a></li>
+                <li><a href="change_password_instructor.html">Edit Account</a></li>
+                <li class="selected"><a href="add_new_question_instructor.html">Add New Question</a></li>
+                <li><a href="results_instructor.html">Results</a></li>
+                <li><a href="questions_instructor.html">Questions</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<?php
+    switch ($questionType) {
+        
+        case 'text':
+?>
+    
+    <div id="content">
+    <div id="container">
+         <div class="question_in_review">
+                <h1>Q1 - Section <?php
+            echo $section;
+?>.<?php
+            echo $subSection;
+?></h1>
+                
+                    <form>                    
+                        <div class="bold">Short Answer.</div>
+                        <div><?php
+            echo $question;
+?></div>
+                        <div>
+                            <input type="text" name="answer"/>
+                        </div>
+                    </form>
+                <div class="centered">
+                    <input type="button" name="submit" value="Submit Answer"/>
+                </div>        
+        </div>            
+    </div>            
+    </div> 
+</body>
+</html>
+ 
+<?php
+            break;
+        case 'true_false':
+?>
+   <div id="content">
+     <div id="container">
+     <div class="question_in_review">
+            <h1>Q1 - Section <?php
+            echo $section;
+?>.<?php
+            echo $subSection;
+?></h1>
+            
+                <form>                    
+                    <div class="bold">True or False</div>
+                    <div><?php
+            echo $question;
+?></div>        
+                <div>
+                    <input type="radio" name="truefalse" value="true"> True<br>
+                    <input type="radio" name="truefalse" value="false"> False
+                </div>
+            </form>
+        </div>
+            <div class="centered">
+                <input type="button" name="submit" value="Submit Answer"/>
+            </div>        
+    </div>            
+    </div>            
+</div> 
+</body>
+</html>        
+<?php
+            break;
+        case 'radio':
+?>
+
+<div id="content">
+     <div id="container">
+     <div class="question_in_review">
+            <h1>Q1 - Section <?php
+            echo $section;
+?>.<?php
+            echo $subSection;
+?></h1>
+            
+                <form>                    
+                    <div class="bold">Radio(Select one answer)</div>
+                    <div><?php
+            echo $question;
+?></div>        
+                <div>
+                
+<?php
+            $numAnswers = $_POST['num_of_answers_radio'] + 1;
+            
+            for ($i = 1; $i < $numAnswers; $i++) {
+                $question = $_POST['radio_question' . $i];
+                echo '<input type="radio" name="q1" value="true">' . $question . '<br>';
+            }
+            
+?>                
+                    
+                </div>
+            </form>
+        </div>
+            <div class="centered">
+                <input type="button" name="submit" value="Submit Answer"/>
+            </div>        
+    </div>            
+    </div>            
+</div> 
+</body>
+</html>        
+
+<?php
+            break;
+        case 'checkbox':
+?>
+<div id="content">
+     <div id="container">
+     <div class="question_in_review">
+            <h1>Q1 - Section <?php
+            echo $section;
+?>.<?php
+            echo $subSection;
+?></h1>
+            
+                <form>                    
+                    <div class="bold">Radio(Select one answer)</div>
+                    <div><?php
+            echo $question;
+?></div>        
+                <div>
+                
+<?php
+            $numAnswers = $_POST['num_of_answers_checkbox'] + 1;
+            
+            for ($i = 1; $i < $numAnswers; $i++) {
+                $question = $_POST['checkbox_question' . $i];
+                echo '<input type="checkbox" name="q1" value="true">' . $question . '<br>';
+            }
+            
+?>                
+                    
+                </div>
+            </form>
+        </div>
+            <div class="centered">
+                <input type="button" name="submit" value="Submit Answer"/>
+            </div>        
+    </div>            
+    </div>            
+</div> 
+</body>
+</html>    
+
+
+<?php
+            break;
+            
+    }
+}
+?>
---
+++ Project/web_clicker.js
@@ -0,0 +1,126 @@
+function openTab(evt, userType) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i += 1) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i += 1) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(userType).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+window.onload = function() {
+    document.getElementById("add_radio").style.display = "none";
+    document.getElementById("add_checkbox").style.display = "none";
+    document.getElementById("add_text").style.display = "none";
+    document.getElementById("add_tf").style.display = "none";
+}
+
+function addRadio() {
+    document.getElementById("add_radio").style.display = "block";
+    document.getElementById("add_checkbox").style.display = "none";
+    document.getElementById("add_text").style.display = "none";
+    document.getElementById("add_tf").style.display = "none";
+
+    addRadioOptions();
+}
+
+function addRadioOptions() {
+    var htmlElements = "";
+    var container = document.getElementById("radio_container");
+    container.innerHTML = htmlElements;
+
+    var numOfAnswers = (document.getElementById("num_of_answers_radio").value * 1 + 1);
+
+    for (var i = 1; i < numOfAnswers; i++) {
+        htmlElements += '<input type="text" id="radio' + i + '" name="radio_question' + i + '"' +
+            'placeholder="Enter a answer" ><input type="radio" name="correct_answer" value="q' + i + '" >Correct answer'
+    }
+
+    container.innerHTML += htmlElements;
+}
+
+function addCheckbox() {
+    document.getElementById("add_radio").style.display = "none";
+    document.getElementById("add_checkbox").style.display = "block";
+    document.getElementById("add_text").style.display = "none";
+    document.getElementById("add_tf").style.display = "none";
+
+
+    addCheckBoxOptions();
+}
+
+function addCheckBoxOptions() {
+    var htmlElements = "";
+    var container = document.getElementById("checkbox_container");
+    container.innerHTML = htmlElements;
+
+    var numOfAnswers = (document.getElementById("num_of_answers_checkbox").value * 1 + 1);
+
+    for (var i = 1; i < numOfAnswers; i++) {
+        htmlElements += '<input type="text" id="checkbox' + i + '" name="checkbox_question' + i + '"' +
+            'placeholder="Enter a answer" ><input type="checkbox" name="correct_answer" value="q' + i + '">Correct answer'
+    }
+
+    container.innerHTML += htmlElements;
+}
+
+
+function addText() {
+    document.getElementById("add_radio").style.display = "none";
+    document.getElementById("add_checkbox").style.display = "none";
+    document.getElementById("add_text").style.display = "block";
+    document.getElementById("add_tf").style.display = "none";
+}
+
+function addTF() {
+    document.getElementById("add_radio").style.display = "none";
+    document.getElementById("add_checkbox").style.display = "none";
+    document.getElementById("add_text").style.display = "none";
+    document.getElementById("add_tf").style.display = "block";
+}
+
+function submitPasswordChangeStudent() {
+
+}
+
+function submitPasswordChangeInstructor() {
+
+}
+
+function addNewQuestion() {
+
+}
+
+function searchQuestion() {
+
+}
+
+function displayQuestion(x) {
+    if (document.getElementById("question" + x).style.display == "block") {
+        document.getElementById("question" + x).style.display = "none";
+        document.getElementById("button" + x).innerHTML = "view question";
+    } else {
+        document.getElementById("question" + x).style.display = "block";
+        document.getElementById("button" + x).innerHTML = "hide question";
+    }
+}
+
+function deactivateQuestion(x) {
+
+}
+
+function editQuestion(x) {
+
+}
+
+function deleteQuestion(x) {
+
+}
+
+if (document.getElementById("defaultOpen")) {
+    document.getElementById("defaultOpen").click();
+}
