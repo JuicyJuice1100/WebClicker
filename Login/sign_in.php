0130438f2e796fb26b620a3b992da78a42c1a241
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2017-12-10 11:15:55 -0600

    Fixed calvin

--- Login/sign_in.php
+++ Login/sign_in.php
@@ -29,28 +29,39 @@ if (isset($_POST['submit_instructor'])) {
 		// Establish connection (this is already established)
 		// $connection = mysql_connect("localhost", "team1", "steam1", "team1");
 
-		// protect against SQL injection for security
-		$username = stripslashes($username);
-		$password = stripslashes($password);
-		$username = mysql_real_escape_string($username);
-		$password = mysql_real_escape_string($password);
+    // protect against SQL injection for security
+    // (don't have to worry because we don't )
+		// $username = stripslashes($username);
+		// $password = stripslashes($password);
+		// $username = mysql_real_escape_string($username);
+		// $password = mysql_real_escape_string($password);
 
 		// select database (this is already established)
 		// $db = mysql_select_db("team1", $connection);
 
-		// SQL query to fetch information of registerd users and finds user match.
-		$query = mysql_query("SELECT * FROM Instructor WHERE HashedPassword='$password' AND Username='$username'", $connection);
-		$rows = mysql_num_rows($query);
-		if ($rows == 1) {
-			$_SESSION['login_user']=$username; // initialize session variables
+    // SQL query to fetch information of registerd users and finds user match.
+    
+		// $query = mysql_query("SELECT * FROM Instructor WHERE HashedPassword='$password' AND Username='$username'", $connection);
+		// $rows = mysql_num_rows($query);
+		// if ($rows == 1) {
+		// 	$_SESSION['login_user']=$username; // initialize session variables
+		// 	$_SESSION['instructor']=true;
+		// 	header("location: profile.php"); // redirect to user account type home page
+		// } else {
+		// 	$error = "Error Instructor : username or password is incorrect";
+		// }
+
+	// mysql_close($connection); // close connection
+  
+    $query = getInstructorPasswordByUsername($username);
+    if($query["HashedPassword"] === crypt($password, $query["HashedPassword"]){
+      $_SESSION['login_user']=$username; // initialize session variables
 			$_SESSION['instructor']=true;
 			header("location: profile.php"); // redirect to user account type home page
-		} else {
-			$error = "Error Instructor : username or password is incorrect";
-		}
-
-	mysql_close($connection); // close connection
-	
+    } else {
+      $error = "Error Instrucotr : username or password is incoreect";
+    }
+    
 	}
 	
 }
@@ -65,30 +76,38 @@ if (isset($_POST['submit_student'])) {
 		$username=$_POST['username_student'];
 		$password=$_POST['password_student'];
 
-		// Establish connection
-		$connection = mysql_connect("localhost", "team1", "steam1", "team1");
+		// // Establish connection
+		// $connection = mysql_connect("localhost", "team1", "steam1", "team1");
 
-		// protect against SQL injection for security
-		$username = stripslashes($username);
-		$password = stripslashes($password);
-		$username = mysql_real_escape_string($username);
-		$password = mysql_real_escape_string($password);
+		// // protect against SQL injection for security
+		// $username = stripslashes($username);
+		// $password = stripslashes($password);
+		// $username = mysql_real_escape_string($username);
+		// $password = mysql_real_escape_string($password);
 
-		// select database
-		$db = mysql_select_db("team1", $connection);
+		// // select database
+		// $db = mysql_select_db("team1", $connection);
 
-		// SQL query to fetch information of registerd users and finds user match.
-		$query = mysql_query("SELECT * FROM Student WHERE HashedPassword='$password' AND Username='$username'", $connection);
-		$rows = mysql_num_rows($query);
-			if ($rows == 1) {
-			$_SESSION['login_user']=$username; // initialize session variables
-			$_SESSION['instructor']=false;
-			header("location: profile.php"); // redirect to user account type home page
-		} else {
-			$error = "Error Student : username or password is incorrect";
-		}
+		// // SQL query to fetch information of registerd users and finds user match.
+		// $query = mysql_query("SELECT * FROM Student WHERE HashedPassword='$password' AND Username='$username'", $connection);
+		// $rows = mysql_num_rows($query);
+		// 	if ($rows == 1) {
+		// 	$_SESSION['login_user']=$username; // initialize session variables
+		// 	$_SESSION['instructor']=false;
+		// 	header("location: profile.php"); // redirect to user account type home page
+		// } else {
+		// 	$error = "Error Student : username or password is incorrect";
+		// }
 
-	mysql_close($connection); // close connection
+  // mysql_close($connection); // close connection
+  $query = getStudentPasswordByUsername($username);
+  if($query["HashedPassword"] === crypt($password, $query["HashedPassword"]){
+    $_SESSION['login_user']=$username; // initialize session variables
+    $_SESSION['instructor']=true;
+    header("location: profile.php"); // redirect to user account type home page
+  } else {
+    $error = "Error Student : username or password is incoreect";
+  }
 	}
 }
 
